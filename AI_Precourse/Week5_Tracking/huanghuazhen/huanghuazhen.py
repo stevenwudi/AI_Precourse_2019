c8782d79f46796919767dd346b896209bd2eb723
Ushio_ <13602684930@163.com>
2019-05-22 18:35:17 +0800

    add huanghuazhen's resubmitted assignment5

--- AI_Precourse/Week5_Tracking/huanghuazhen/huanghuazhen.py
+++ AI_Precourse/Week5_Tracking/huanghuazhen/huanghuazhen.py
@@ -1,45 +1,93 @@
-# This script will detect faces via your webcam.
-# Tested with OpenCV3
-
 import cv2
+import sys
+ 
 
-cap = cv2.VideoCapture(0)
+if __name__ == '__main__' :
+ 
+    # Set up tracker.
+    # Instead of MIL, you can also use
+ 
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[2]
+ 
+    if tracker_type == 'BOOSTING':
+        tracker = cv2.TrackerBoosting_create()
+    if tracker_type == 'MIL':
+        tracker = cv2.TrackerMIL_create()
+    if tracker_type == 'KCF':
+        tracker = cv2.TrackerKCF_create()
+    if tracker_type == 'TLD':
+        tracker = cv2.TrackerTLD_create()
+    if tracker_type == 'MEDIANFLOW':
+        tracker = cv2.TrackerMedianFlow_create()
+    if tracker_type == 'GOTURN':
+        tracker = cv2.TrackerGOTURN_create()
+    if tracker_type == 'MOSSE':
+        tracker = cv2.TrackerMOSSE_create()
+    if tracker_type == "CSRT":
+        tracker = cv2.TrackerCSRT_create()
+    # Read video
+    video = cv2.VideoCapture("SethCurry.mp4")
 
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-i=1
-while(True):
-	# Capture frame-by-frame
-	ret, frame = cap.read()
+    # Exit if video not opened.
+    if not video.isOpened():
+        print ("Could not open video")
+        sys.exit()
+ 
+    # Read first frame.
+    ret, frame = video.read()
+    if not ret:
+        print ("Cannot read video file")
+        sys.exit()
+     
 
-	# Our operations on the frame come here
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+     
+    # Define an initial bounding box
+    bbox = (287, 23, 86, 320)
+ 
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
 
-	# Detect faces in the image
-	faces = faceCascade.detectMultiScale(
-		gray,
-		scaleFactor=1.1,
-		minNeighbors=5,
-		minSize=(30, 30)
-		#flags = cv2.CV_HAAR_SCALE_IMAGE
-	)
-
-	print("Found {0} faces!".format(len(faces)))
-
-	# Draw a rectangle around the faces
-	for (x, y, w, h) in faces:
-		frame=cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-	
-	cv2.imwrite("./frame/frame_%d.jpg"%i,frame)
-	i=i+1
-	if i>3:
-		break
-
-	# Display the resulting frame
-	cv2.imshow('frame', frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # Initialize the tracker with a known bounding box that surrounded the target
+    ret = tracker.init(frame, bbox)
+    i=1
+    while True:
+        # Read a new frame
+        ret, frame = video.read()
+        if not ret:
+            break
+         
+        # Start timer,用于返回从操作系统启动到当前所经的计时周期数
+        timer = cv2.getTickCount()
+ 
+        # Update tracker
+        ret, bbox = tracker.update(frame)
+ 
+        # Calculate Frames per second (FPS)
+        #cv2.getTickFrequency(),用于返回CPU的频率
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+ 
+        # Draw bounding box
+        if ret:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+ 
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+     
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+        
+        if i<=3:
+            cv2.imwrite("./frame/frame_%d.jpg"%i,frame)
+        i=i+1
+        # Display result
+        cv2.imshow("Tracking", frame)
+        # Exit if ESC pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
